9cdc3e0609f26f5ae6ec891a5cf12a151472ecea
David <84243957+blend3d@users.noreply.github.com>
2026-05-26 16:00:19 -0400

    Add file

--- Code/Snippets/WIP_button_press.py
+++ Code/Snippets/WIP_button_press.py
@@ -1,13 +1,24 @@
-from inventor import Inventor
+from inventor import Inventor, MOTOR_A, MOTOR_B
 
 board = Inventor()
 
-print("Press USER button to start...")
+# print("Press USER button to start...")
+# 
+# # Wait here until the button is pressed
+# while not board.switch_pressed():	
+#     pass
+# 
+# print("Starting main program!")
+# 
+# # --- Your main code here ---
+# print("Press USER button to start...")
+# 
+# # Wait here until the button is pressed
+# while not board.switch_pressed():	
+#     pass
 
-# Wait here until the button is pressed
-while not board.switch_pressed():
-    pass
-
-print("Starting main program!")
-
-# --- Your main code here ---
+#help(Inventor)
+print(board.switch_pressed())   # Note the parentheses
+capture = board.encoders[MOTOR_A].capture()
+help(board.encoders[MOTOR_A].capture())
+print(board.encoder_a.capture())
