eaba525b94b748ea80beb03020a1957a4ef5ac6c
David <84243957+blend3d@users.noreply.github.com>
2026-05-26 21:17:04 -0400

    update

--- Code/Snippets/WIP_button_press_with_led_blink.py
+++ Code/Snippets/WIP_button_press_with_led_blink.py
@@ -1,7 +1,9 @@
 from inventor import Inventor
 import time
+import utime
 
 board = Inventor()
+led_onboard = machine.Pin("LED", machine.Pin.OUT)
 
 print("Press USER button to start...")
 
@@ -11,12 +13,13 @@ print("Press USER button to start...")
 try:
     while not board.switch_pressed():	
         # Run "pass" loop until the user button is pressed
+        led_onboard.value(1)
         pass
 
 finally:
     # Exit "pass" loop, print message, and insert time delay for switch bounce to settle
     print("USER button pressed, starting code...")
-    time.sleep(0.4)		# Time delay of 0.4 seconds
+    # time.sleep(0.4)		# Time delay of 0.4 seconds
 
 #---------------------------------
 # Run code until the user button is pressed
@@ -24,7 +27,10 @@ finally:
 try:
     # Run code until the user button is pressed
     while not board.switch_pressed():
-        print(board.encoder_a.capture())
+        led_onboard.value(1)
+        utime.sleep(0.1)
+        led_onboard.value(0)
+        utime.sleep(1.75)
 
 finally:
     # Run Code and Exit
